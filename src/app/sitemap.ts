25b30ced6db3a1c7252c5b120be444c8400b618b
Spark960 <lokeshparasa50@gmail.com>
2026-05-25 16:38:21 +0530

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://lokesh.dev",
+      url: "https://portfolio-loki-one.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
